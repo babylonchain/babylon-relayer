4288bfb1fd64e77d3066144e8621216ea9a50281
Runchao Han <me@runchao.rocks>
2023-01-24 22:51:32 +1100

    fix: automatically calculate TrustingPeriod and override previous light clients (#9)

--- bbnrelayer/client.go
+++ bbnrelayer/client.go
@@ -10,7 +10,6 @@ import (
 	"github.com/cosmos/relayer/v2/relayer"
 	"github.com/cosmos/relayer/v2/relayer/provider"
 	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
 )
 
 // Relayer is a relayer that allows to relay multiple chains concurrently.
@@ -24,90 +23,6 @@ func New(logger *zap.Logger) *Relayer {
 	return &Relayer{
 		logger: logger,
 	}
-}
-
-func (r *Relayer) createClientIfNotExist(
-	ctx context.Context,
-	src *relayer.Chain,
-	dst *relayer.Chain,
-	memo string,
-	numRetries uint,
-) error {
-	// query the latest heights on src and dst
-	// retry here in case the CZ endpoint becomes unstable
-	var srch, dsth int64
-	if err := retry.Do(func() error {
-		var err error
-		srch, dsth, err = relayer.QueryLatestHeights(ctx, src, dst)
-		if err != nil {
-			return fmt.Errorf("failed to query latest heights: %w", err)
-		}
-		return nil
-	}, retry.Context(ctx), retry.Attempts(numRetries), relayer.RtyDel, relayer.RtyErr); err != nil {
-		return err
-	}
-
-	// check whether the dst light client exists on src at the latest height
-	// if err is nil, then the client exists, return directly
-	if _, err := src.ChainProvider.QueryClientState(ctx, srch, src.PathEnd.ClientID); err == nil {
-		r.logger.Info(
-			"the light client already exists. Skip creating the light client.",
-			zap.String("src_chain_id", src.ChainID()),
-			zap.String("dst_chain_id", dst.ChainID()),
-			zap.String("dst_client_id", src.PathEnd.ClientID),
-		)
-		return nil
-	}
-
-	// if the code reaches here, then it means the client does not exist
-	// we need to create a new one
-	r.logger.Info(
-		"the light client does not exist. Creating a new light client.",
-		zap.String("src_chain_id", src.ChainID()),
-		zap.String("dst_chain_id", dst.ChainID()),
-		zap.String("dst_client_id", src.PathEnd.ClientID),
-	)
-
-	// Query the light signed headers for src & dst at the heights srch & dsth
-	var srcUpdateHeader, dstUpdateHeader provider.IBCHeader
-	if err := retry.Do(func() error {
-		var err error
-		srcUpdateHeader, dstUpdateHeader, err = relayer.QueryIBCHeaders(ctx, src, dst, srch, dsth)
-		if err != nil {
-			return fmt.Errorf("failed to query update headers: %w", err)
-		}
-		return nil
-	}, retry.Context(ctx), retry.Attempts(numRetries), relayer.RtyDel, relayer.RtyErr); err != nil {
-		return err
-	}
-
-	// create the client on src chain, where we use default values for some fields
-	// TODO: allow custom TrustingPeriod
-	r.Lock()
-	if _, err := relayer.CreateClient(
-		ctx,
-		src,
-		dst,
-		srcUpdateHeader,
-		dstUpdateHeader,
-		true, // allowUpdateAfterExpiry
-		true, // allowUpdateAfterMisbehaviour
-		true, // override
-		0,    // customClientTrustingPeriod
-		memo,
-	); err != nil {
-		return err
-	}
-	r.Unlock()
-
-	r.logger.Info(
-		"successfully created the light client",
-		zap.String("src_chain_id", src.ChainID()),
-		zap.String("dst_chain_id", dst.ChainID()),
-		zap.String("dst_client_id", src.PathEnd.ClientID),
-	)
-
-	return nil
 }
 
 // UpdateClient updates the IBC light client on src chain that tracks dst chain given the configured path
@@ -132,15 +47,9 @@ func (r *Relayer) UpdateClient(
 		return err
 	}
 
-	var srcMsgUpdateClient provider.RelayerMessage
-	eg, egCtx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		var err error
-		srcMsgUpdateClient, err = relayer.MsgUpdateClient(egCtx, dst, src, dsth, srch)
-		return err
-	})
-
-	if err := eg.Wait(); err != nil {
+	// generate MsgUpdateClient that carries dst header to src
+	srcMsgUpdateClient, err := relayer.MsgUpdateClient(ctx, dst, src, dsth, srch)
+	if err != nil {
 		return err
 	}
 
