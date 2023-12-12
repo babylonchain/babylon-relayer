4682e1ea0fcb779f406d750a27eb243268107409
Vitalis Salis <VitSalis@gmail.com>
2023-12-12 09:01:48 +0400

    fix: Golang 1.21 on Dockerfile and CircleCI (#2)

--- bbnrelayer/msg.go
+++ bbnrelayer/msg.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/avast/retry-go/v4"
-	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
+	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types" //nolint:staticcheck
 	ibcexported "github.com/cosmos/ibc-go/v8/modules/core/exported"
 	"github.com/cosmos/relayer/v2/relayer"
 	"github.com/cosmos/relayer/v2/relayer/provider"
